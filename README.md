ad834e1ed9dfcdaa92c91532022ab5836dfed32c
Seiken <dubroca.axel@gmail.com>
2016-03-28 23:09:30 +0200

    Omelette du fromage
    Le premier commit n'est malheureusement pas le projet vierge. Je le fais d'habitude, mais j'ai oublié. Donc le voici.
    Excepté l'automate du mélange, tous les tests passent. Cela ne veut pas dire que le code est parfait, simplement qu'il fonctionne dans la plupart des cas. Il faut encore essayer les cas plus tordus, en ajoutant de nouveaux tests. Ce sera à faire, mais il faut d'abord s'occuper de cet automate du mélange, au moins pour montrer que le principe est compris.
    Pour cette fonction, il faut créer un automate qui combine astucieusement les deux autres, afin qu'un algorithme de déterminisation fasse le travail. Si je ne me trompe pas, c'est comme l'union de deux langages, mais pour chaque état, entrer une "mauvaise lettre" fait rester sur le même état. Ce n'est pas forcément clair dit comme ça. Mais visuellement, l'algorithme est simple à appliquer, il faut maintenant trouver comment l'implémenter. Il va falloir justement s'inspirer de la déterminisation.
    
    Enfin bref, voilà pour ce commit.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Automate-du-fromage
+
+Quand l'automate du mélange a des airs de French Touch.
+
+## Official lyrics
+
+J'n'ai pas choisi de l'faire
+Mais c'est là, "L'Incodablemento"
+Les sources, le code, peut-être
+Mais confondre le graphe pour un flot
+Tout se compile et c'est bien tout
+Et c'est là, "L'Incodablemento"
+Tout les SEGFAULT s'imposent à nous
+Trouver enfin peut-être un errno
+
+## Compile
+
+Avec "make".
+
+## Test
+
+Avec "make check".
+
+## Bake cookies
+
+Avec "make cookies".
+
+## You got a letter
+
+From who ?
+
+## From whom.
+
+What ?
+
+## Sign here.
+
+
+
+
+
+
+
+(Just here)
---
+++ tests.mk
@@ -0,0 +1,11 @@
+tests/test_miroir: tests/test_miroir.o libautomate.a
+tests/test_delta_delta_star: tests/test_delta_delta_star.o libautomate.a
+tests/test_automate_accessible: tests/test_automate_accessible.o libautomate.a
+tests/test_translater_etat: tests/test_translater_etat.o libautomate.a
+tests/test_get_max_etat: tests/test_get_max_etat.o libautomate.a
+tests/test_creer_automate: tests/test_creer_automate.o libautomate.a
+tests/test_automate_vide: tests/test_automate_vide.o libautomate.a
+tests/test_ensemble: tests/test_ensemble.o libautomate.a
+tests/test_automate_du_melange: tests/test_automate_du_melange.o libautomate.a
+tests/test_table: tests/test_table.o libautomate.a
+
